777f2b48069be4fd0850749e61428e9bf0d86a1d
Daniel Spajic <danieljspajic@gmail.com>
2018-03-28 16:38:53 +1030

    Fix ESLint errors
    - Add missing semicolon
    - Disable `import/no-extraneous-dependencies` rule in Webpack config (it
    shouldn't be used here)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 const BundleTracker = require('webpack-bundle-tracker');
 const merge = require('webpack-merge');
-const path = require('path');
 const webpack = require('webpack');
 
 const common = require('./webpack.common.js');
 
 const devServerPort = 8080;
-const devServerHost = '0.0.0.0'
+const devServerHost = '0.0.0.0';
 const devServerUrl = `http://${devServerHost}:${devServerPort}`;
 const devServerPublicPath = `${devServerUrl}/static/`;
 
