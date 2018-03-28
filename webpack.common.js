95469f9a2d0bfea1accf42a40d0c2a209565a70c
Daniel Spajic <danieljspajic@gmail.com>
2018-03-28 17:40:12 +1030

    Configure Webpack to resolve image files

--- webpack.common.js
+++ webpack.common.js
@@ -72,6 +72,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
