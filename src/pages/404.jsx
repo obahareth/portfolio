1cfcb4c4d1929066096a745bd8e68b33727e4c8a
Daniel Spajic <9282837+dspacejs@users.noreply.github.com>
2018-09-15 20:06:17 +0930

    Update 404 page design
    - Add gif and improve page styles/content
    - Update tests for changes

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,36 @@
+import Link from 'gatsby-link';
 import React from 'react';
+import { Container } from 'reactstrap';
+
+import Heading from 'components/Heading';
 
 const NotFoundPage = () => (
-  <section>
-    <h1>
-      NOT FOUND
-    </h1>
-    <p>
-      You just hit a route that doesn&#39;t exist... the sadness.
-    </p>
+  <section className="pt-5 text-center">
+    <Container>
+      <Heading size={1}>
+        <span className="text-primary">
+          404
+        </span>
+        {' '}
+        NOT FOUND
+      </Heading>
+      <p>
+        This page doesn&apos;t exist.
+        {' '}
+        <Link to="/">
+          Go to the home page
+        </Link>
+      </p>
+      <iframe
+        title="404 gif"
+        src="https://giphy.com/embed/l1J9EdzfOSgfyueLm"
+        width="480"
+        height="480"
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen
+      />
+    </Container>
   </section>
 );
 
--- src/pages/__tests__/404.test.jsx
+++ src/pages/__tests__/404.test.jsx
@@ -2,12 +2,16 @@ import NotFoundPage from '../404';
 
 describe("<NotFoundPage>", () => {
   let mountedComponent;
+
   const getComponent = () => {
     if (!mountedComponent) {
       mountedComponent = shallow(<NotFoundPage />);
     }
     return mountedComponent;
   };
+  const getHeading = () => getComponent().find('Heading');
+  const getHomePageLink = () => getComponent().find("GatsbyLink[to='/']");
+  const getGif = () => getComponent().find("iframe[src*='giphy.com']");
 
   beforeEach(() => {
     mountedComponent = undefined;
@@ -15,5 +19,21 @@ describe("<NotFoundPage>", () => {
 
   it("renders a <section> as the root element", () => {
     expect(getComponent().is('section')).toBeTruthy();
+  });
+
+  it("renders a <Heading>", () => {
+    expect(getHeading()).toHaveLength(1);
+  });
+
+  it("renders a <Link> to the home page", () => {
+    expect(getHomePageLink()).toHaveLength(1);
+  });
+
+  it("renders a <Link> to the home page", () => {
+    expect(getHomePageLink()).toHaveLength(1);
+  });
+
+  it("renders a gif", () => {
+    expect(getGif()).toHaveLength(1);
   });
 });
