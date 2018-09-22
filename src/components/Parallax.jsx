65ea52d0a25d2b00a8b04784eaf8c0343dabc80b
Daniel Spajic <9282837+dspacejs@users.noreply.github.com>
2018-09-22 19:06:07 +0930

    Fix Parallax.jsx during build
    - Move `isSmallScreen` calculation into `componentDidMount`. `window` is
    undefined during build; this ensures it's only run in the browser

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -2,35 +2,56 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Plx from 'react-plx';
 
-const Parallax = ({ children }) => {
-  const isSmallScreen = window.innerHeight < 900;
+class Parallax extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      easing: undefined,
+      time: 500,
+    };
+  }
 
-  const parallaxData = [
-    {
-      start: 0,
-      end: isSmallScreen ? 600 : 500,
-      easing: isSmallScreen ? 'easeIn' : undefined,
-      properties: [
-        {
-          startValue: 1,
-          endValue: 0,
-          property: 'opacity',
-        },
-        {
-          startValue: 1,
-          endValue: -50,
-          property: 'translateY',
-        },
-      ],
-    },
-  ];
+  componentDidMount() {
+    const isSmallScreen = window.innerHeight < 900;
 
-  return (
-    <Plx parallaxData={parallaxData}>
-      {children}
-    </Plx>
-  );
-};
+    if (isSmallScreen) {
+      this.setState({
+        easing: 'easeIn',
+        time: 600,
+      });
+    }
+  }
+
+  render() {
+    const { easing, time } = this.state;
+    const { children } = this.props;
+    const parallaxData = [
+      {
+        start: 0,
+        end: time,
+        easing,
+        properties: [
+          {
+            startValue: 1,
+            endValue: 0,
+            property: 'opacity',
+          },
+          {
+            startValue: 1,
+            endValue: -50,
+            property: 'translateY',
+          },
+        ],
+      },
+    ];
+
+    return (
+      <Plx parallaxData={parallaxData}>
+        {children}
+      </Plx>
+    );
+  }
+}
 
 Parallax.propTypes = {
   children: PropTypes.node.isRequired,
